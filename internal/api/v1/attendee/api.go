2c8c338cfb0308709c404d349873b43ed5a6b679
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2022-10-23 14:57:10 +0200

    #50 - fix openapi and add model structs

--- internal/api/v1/attendee/api.go
+++ internal/api/v1/attendee/api.go
@@ -18,6 +18,7 @@ type AttendeeDto struct {
 	Email    string `json:"email"`
 	Phone    string `json:"phone"`
 	Telegram string `json:"telegram"`
+	Partner  string `json:"partner"`
 
 	// personal data
 	Birthday   string `json:"birthday"` // ISO date (format yyyy-MM-dd)
@@ -36,4 +37,72 @@ type AttendeeDto struct {
 
 type AttendeeMaxIdDto struct {
 	MaxId uint `json:"max_id"`
+}
+
+type AttendeeIdList struct {
+	Ids []int64 `json:"ids"`
+}
+
+// --- search criteria ---
+
+type AttendeeSearchCriteria struct {
+	MatchAny   []AttendeeSearchSingleCriterion `json:"match_any"`
+	MinId      int64                           `json:"min_id"`
+	MaxId      int64                           `json:"max_id"`
+	NumResults int64                           `json:"num_results"`
+	FillFields []string                        `json:"fill_fields"`
+	SortBy     string                          `json:"sort_by"`
+	SortOrder  string                          `json:"sort_order"`
+}
+
+type AttendeeSearchSingleCriterion struct {
+	Ids          []int64          `json:"ids,omitempty"`
+	Nickname     string           `json:"nickname"`
+	Name         string           `json:"name"`
+	Address      string           `json:"address"`
+	Country      string           `json:"country"`
+	CountryBadge string           `json:"country_badge"`
+	Email        string           `json:"email"`
+	Telegram     string           `json:"telegram"`
+	Flags        map[string]int64 `json:"flags"`
+	Options      map[string]int64 `json:"options"`
+	Packages     map[string]int64 `json:"packages"`
+	UserComments string           `json:"user_comments"`
+}
+
+// --- search result ---
+
+type AttendeeSearchResultList struct {
+	Attendees []AttendeeSearchResult `json:"attendees"`
+}
+
+type AttendeeSearchResult struct {
+	Id             int64   `json:"id"`
+	BadgeId        *string `json:"badge_id,omitempty"`
+	Nickname       *string `json:"nickname,omitempty"`
+	FirstName      *string `json:"first_name,omitempty"`
+	LastName       *string `json:"last_name,omitempty"`
+	Street         *string `json:"street,omitempty"`
+	Zip            *string `json:"zip,omitempty"`
+	City           *string `json:"city,omitempty"`
+	Country        *string `json:"country,omitempty"`
+	CountryBadge   *string `json:"country_badge,omitempty"`
+	State          *string `json:"state,omitempty"`
+	Email          *string `json:"email,omitempty"`
+	Phone          *string `json:"phone,omitempty"`
+	Telegram       *string `json:"telegram,omitempty"`
+	Partner        *string `json:"partner,omitempty"`
+	Birthday       *string `json:"birthday,omitempty"`
+	Gender         *string `json:"gender,omitempty"`
+	Pronouns       *string `json:"pronouns,omitempty"`
+	TshirtSize     *string `json:"tshirt_size,omitempty"`
+	Flags          *string `json:"flags,omitempty"`
+	Options        *string `json:"options,omitempty"`
+	Packages       *string `json:"packages,omitempty"`
+	UserComments   *string `json:"user_comments,omitempty"`
+	Status         *string `json:"status,omitempty"`
+	TotalDues      *int64  `json:"total_dues,omitempty"`
+	PaymentBalance *int64  `json:"payment_balance,omitempty"`
+	CurrentDues    *int64  `json:"current_dues,omitempty"`
+	DueDate        *string `json:"due_date,omitempty"`
 }
