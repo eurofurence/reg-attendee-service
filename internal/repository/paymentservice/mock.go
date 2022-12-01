2dee8e91ff5b25f78a0e80930df3ee6309a6b447
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2022-12-01 11:29:46 +0100

    issue #62 - log locally added transactions if downstream disabled

--- internal/repository/paymentservice/mock.go
+++ internal/repository/paymentservice/mock.go
@@ -2,6 +2,7 @@ package paymentservice
 
 import (
 	"context"
+	aulogging "github.com/StephanHCB/go-autumn-logging"
 )
 
 type Mock interface {
@@ -54,6 +55,8 @@ func (m *MockImpl) AddTransaction(ctx context.Context, transaction Transaction) 
 
 	_ = m.InjectTransaction(ctx, transaction)
 	m.recording = append(m.recording, transaction)
+
+	aulogging.Logger.Ctx(ctx).Info().Printf("add transaction debitor %d type %s status %s method %s for %0.2f %s", transaction.DebitorID, transaction.Type, transaction.Status, transaction.Method, float64(transaction.Amount.GrossCent)/100.0, transaction.Amount.Currency)
 
 	return nil
 }
